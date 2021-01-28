b59224b96e03251f89eb8dbea2ae1fa259a0f72e
amit <amitoosh.mor@gmail.com>
2021-01-28 10:59:23 +0200

    sort

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ const addButton = document.getElementById("add-button");
 let input = document.getElementById("text-input");
 let viewSection = document.getElementById("view-section");
 let priorityNum = document.getElementById("priority-selector");
-let qounter = 0;
+let qounter = document.getElementById("counter");
+let qount =0;
 addButton.addEventListener("click", addTodo );
+let arrOfPriority=[];
 function addTodo(event){
     
 
@@ -21,12 +23,65 @@ function addTodo(event){
     todoText.innerText = input.value;
     viewSection.append(todoContainer);
     priority.innerText= priorityNum.value;
+    
+    // arrOfPriority.push(priorityNum.value);
+
+    
+    
     input.innerText="";
     input.value="";
     // priority.value="";
-    qounter++;
-     
+    
+    qount++;
+    qounter.innerText=qount;
+    qounter.value=qount;
+    
+    
     
 }
-let priorities = document.querySelectorAll("todo-priority");
-console.log(priorities);
+
+const sortButton = document.getElementById("sort-button");
+sortButton.addEventListener("click",sorting);
+function sorting(event){
+    let priorities = document.getElementsByClassName("todo-priority");
+    console.log(priorities);
+    viewSection.innerHTML="";
+    let newPriority= sort(priorities);
+    console.log(newPriority);
+
+    // for (const priority of priorities) {
+    //     arrOfPriority.push(priority.innerText);
+    // }
+    // console.log(arrOfPriority);
+    
+}
+
+
+
+function sort(arr){
+    let finalArr=[];
+    let place=0;
+    for (let i=0 ; i<arr.length ; i++){
+        for(let j = 0 ; j<arr.length-1 ; j++){
+            if(Number(arr[0].innerText)<Number(arr[j+1].innerText)){
+                place++;
+            }
+        }
+        finalArr[place]=arr[0];
+        arr.push(arr.shift());
+        place=0;
+    }
+    return(finalArr);
+}    
+
+
+
+
+
+
+
+
+
+
+// let priorities = document.getElementsByClassName("todo-priority");
+// console.log(priorities);
