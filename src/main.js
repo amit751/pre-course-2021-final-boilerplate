c16f22d8097103c8f3cd44928613dbd7c9c898b4
amit <amitoosh.mor@gmail.com>
2021-01-31 11:13:28 +0200

    mess

--- src/main.js
+++ src/main.js
@@ -1,47 +1,38 @@
-
 function newElement( element , clas , content , appendTo){
     const x = document.createElement(element);
     x.classList.add(clas);
     x.innerText=content;
     appendTo.append(x);
-
     return x;
-
-
 }
-
-
 const list = catchElement("list");
-
 let qounter = catchElement("counter");
-
 let qount =Number(localStorage.getItem("qounter"));
 qounter.innerText=qount;
-
 function catchElement(id){
     const x = document.getElementById(id);
     return x;
 }
-
 window.onload = function(){
     if(JSON.parse(localStorage.getItem("todosObjects"))){
         const previusObj = JSON.parse(localStorage.getItem("todosObjects"));
         for (const obj of previusObj){
             if(obj.status!== "deleted"){
+
                 const listItem = newElement( "li" , "list-item" , "" , list);
-                listItem.classList.add(obj.liClass);
                 const todoContainer = newElement( "div" , "todo-container" , "" , listItem);
-                const priority = newElement( "span" , "todo-priority" , obj.priority , todoContainer);//span
                 const createdAt = newElement( "span" , "todo-created-at" , obj.date , todoContainer);//span
                 const todoText = newElement( "span" , "todo-text" , obj.text , todoContainer);//span
+                const priority = newElement( "span" , "todo-priority" , obj.priority , todoContainer);//span
                 const buttonsContainer = newElement( "span" , "buttons-container" , "" ,todoContainer );
                 const deletButton = newElement( "button" , "delete" , "delete" ,buttonsContainer); //new button-pass the test
                 const markButton = newElement( "button" , "mark" , "mark" ,buttonsContainer); //new button-pass the test
                 deletButton.addEventListener("click" , deleting ); //n
                 markButton.addEventListener("click" , marking ); //n
+                readingContentFromObj(obj ,todoContainer,listItem);
+                addingButtons(todoContainer);
             }
-           
-            
+
 
         }
        
@@ -52,6 +43,7 @@ window.onload = function(){
 
 
 function deleting(event){  //new pass
+function deleting(event){  
     for (const obj of todosObjects) {
         console.log(obj);
         console.log(obj.innerHTML);
@@ -66,15 +58,12 @@ function deleting(event){  //new pass
     --qount;
     qounter.innerText=qount;
     localStorage.setItem("qounter" ,qount);
-
-
 }
-
-
 const addButton = catchElement("add-button");
 let input = catchElement("text-input");
 let viewSection = catchElement("view-section");
 let priorityNum = catchElement("priority-selector");  //num
+let priorityNum = catchElement("priority-selector");  
 
 const sortButton = catchElement("sort-button");
 
@@ -82,53 +71,53 @@ addButton.addEventListener("click", addTodo );
 sortButton.addEventListener("click",sorting2);
 let todosObjects=[];
 let witchIcon;
-
-
-
-
 if(JSON.parse(localStorage.getItem("todosObjects"))){
      todosObjects=  JSON.parse(localStorage.getItem("todosObjects"));   
 }else{ todosObjects=[];
 }
 
 function addTodo(event){
+
     const listItem = newElement( "li" , "list-item" , "" , list);
     listItem.classList.add(witchIcon);////////////////////////////////////////////////////////////
+    listItem.classList.add(witchIcon);
     const todoContainer = newElement( "div" , "todo-container" , "" ,  listItem);
-    const priority = newElement( "span" , "todo-priority" , priorityNum.value , todoContainer); //num
     const createdAt = newElement( "span" , "todo-created-at" , new Date().toLocaleString().replace('.', '-').replace('.', '-').replace(',', ' ') , todoContainer);
     const todoText = newElement( "span" , "todo-text" , input.value , todoContainer);
+    const priority = newElement( "span" , "todo-priority" , priorityNum.value , todoContainer); //num
     const buttonsContainer = newElement( "span" , "buttons-container" , "" ,todoContainer );
     const deletButton = newElement( "button" , "delete" , "delete" ,buttonsContainer); //new button-pass the test
     const markButton = newElement( "button" , "mark" , "mark" ,buttonsContainer); //new button-pass the test
     deletButton.addEventListener("click" , deleting ); //n
     markButton.addEventListener("click" , marking ); //n
     console.log(todoContainer.innerHTML);
-               
+    const priority = newElement( "span" , "todo-priority" , priorityNum.value , todoContainer); 
+    addingButtons(todoContainer);
+
+
+
 
 
 
     const todoObj ={};
     todoObj.text =  input.value;
     todoObj.priority = priorityNum.value; //num
+    todoObj.priority = priorityNum.value; 
     todoObj.date =  new Date().toLocaleString().replace('.', '-').replace('.', '-').replace(',', ' ') ;
     todoObj.containerInnerHtml = todoContainer.innerHTML;
     todoObj.liClass = witchIcon;
     todosObjects.push(todoObj);
-    
+
     let todosObjsJason=JSON.stringify(todosObjects);
     localStorage.clear();
     localStorage.setItem("todosObjects" , todosObjsJason);
     
     
    
-
-
     input.value="";
     qount++;
     qounter.innerText=qount;
     localStorage.setItem("qounter" ,qount);
-
     
     
     
@@ -143,17 +132,10 @@ function marking(event){
     
     
 }
-
-
-
-
-
-
 function sort(arr){
     const newArr=[];
     for(let i =5 ; i>0 ; i--){
         for (const obj of arr) {
-
             if( obj.priority === i.toString()){
                 newArr.push(obj);
                 
@@ -163,8 +145,6 @@ function sort(arr){
     
     return newArr;
 }
-
-
 function sorting2(){
     
     const arrey = sort(todosObjects);
@@ -173,22 +153,23 @@ function sorting2(){
     for (const obj of arrey) {
         if(obj.status!== "deleted"){
             const listItem = newElement( "li" , "list-item" , "" , list);
-            listItem.classList.add(obj.liClass);
             const todoContainer = newElement( "div" , "todo-container" , "" , listItem);
-            const priority = newElement( "span" , "todo-priority" , obj.priority , todoContainer);
             const createdAt = newElement( "span" , "todo-created-at" , obj.date , todoContainer);
             const todoText = newElement( "span" , "todo-text" , obj.text , todoContainer);
+            const priority = newElement( "span" , "todo-priority" , obj.priority , todoContainer);
             const buttonsContainer = newElement( "span" , "buttons-container" , "" ,todoContainer );
             const deletButton = newElement( "button" , "delete" , "delete" ,buttonsContainer); //new button-pass the test
             const markButton = newElement( "button" , "mark" , "mark" ,buttonsContainer); //new button-pass the test
             deletButton.addEventListener("click" , deleting ); //n
             markButton.addEventListener("click" , marking ); //n
+            addingButtons(todoContainer);
+            readingContentFromObj(obj, todoContainer , listItem);
+
         }
-            
+
     }
     
 }
-
 const icon1 =  catchElement("img1");
 const icon2 =  catchElement("img2");
 const icon3 =  catchElement("img3");
@@ -217,42 +198,20 @@ function choseIcon(event){
     }
 }
 
+function addingButtons(todoContainer){
+
+    const buttonsContainer = newElement( "span" , "buttons-container" , "" ,todoContainer );
+    const deletButton = newElement( "button" , "delete" , "delete" ,buttonsContainer); 
+    const markButton = newElement( "button" , "mark" , "mark" ,buttonsContainer); 
+    deletButton.addEventListener("click" , deleting ); 
+    markButton.addEventListener("click" , marking );    
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
+function readingContentFromObj(obj, todoContainer,listItem){
+    const createdAt = newElement( "span" , "todo-created-at" , obj.date , todoContainer);
+    const todoText = newElement( "span" , "todo-text" , obj.text , todoContainer);
+    const priority = newElement( "span" , "todo-priority" , obj.priority , todoContainer);
+    listItem.classList.add(obj.liClass);
+} 
